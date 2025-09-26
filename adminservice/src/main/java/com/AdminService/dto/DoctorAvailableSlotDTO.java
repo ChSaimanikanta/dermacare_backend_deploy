1d04a8b9c5f21c7a4b8ea3939ec4490972e26555
Sai Manikanta <ch.saimanikanta92@gmai.com>
2025-09-26 11:13:50 +0530

    update

--- adminservice/src/main/java/com/AdminService/dto/DoctorAvailableSlotDTO.java
+++ adminservice/src/main/java/com/AdminService/dto/DoctorAvailableSlotDTO.java
@@ -1,4 +1,4 @@
-package com.clinicadmin.dto;
+package com.AdminService.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class DoctorAvailableSlotDTO {
     private String slot;          // Time like 10:30AM, 11:00AM
     private boolean slotbooked;   // true if booked
